4a929b0a461981a931761a70db32269c6a7f712d
jonathanbirila <jonathanbirila9@gmail.com>
2023-08-24 14:14:04 +0700

    baru

--- database/seeders/DataDummy.php
+++ database/seeders/DataDummy.php
@@ -15,16 +15,10 @@ class DataDummy extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-<<<<<<< HEAD
-            'nama' => 'Operator',
-            'email' => 'operator@gmail.com',
-            'password' => Hash::make('operator'),
-=======
             'nama' => 'Jonathan',
             'email' => 'jonathan@gmail.com',
             'password' => Hash::make('jonathan'),
->>>>>>> f257a199979c8112b6f31ccd12f82344fc07dcbe
-            'role' => 'operator'
+            'role' => 'guest'
         ]);
     }
 }
