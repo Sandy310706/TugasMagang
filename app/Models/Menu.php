5544f688697ac272c8e348ce0f6705d2feec1d63
Robinsilalahi <silalahirobin0@gmail.com>
2023-08-30 14:01:58 +0700

    databaserelasi

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -4,14 +4,39 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Menu extends Model
 {
     use HasFactory;
+
+    protected $table = 'menus';
 
     protected $fillable = [
         'nama',
         'harga',
         'deskripsi'
     ];
+
+    public function Nama_makanans():BelongsTo
+    {
+        return $this->belongsTo(Nama_makanan::class);
+    }
+
+    public function Nama_minumans():BelongsTo
+    {
+        return $this->belongsTo(Nama_minuman::class);
+    }
+
+    public function Pesans():HasMany
+    {
+        return $this->hasMany(Pesan::class);
+    }
+
+    public function Kelola_menus():BelongsTo
+    {
+        return $this->belongsTo(Kelola_menu::class);
+    }
 }
