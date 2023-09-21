580285e3e98ea138424361092f3c363aac9c745e
Sandy310706 <benediktussandy31@gmail.com>
2023-09-21 11:00:10 +0700

    Hapus yajra

--- config/app.php
+++ config/app.php
@@ -159,8 +159,6 @@ return [
         /*
          * Package Service Providers...
          */
-        Yajra\DataTables\DataTablesServiceProvider::class,
-
         /*
          * Application Service Providers...
          */
