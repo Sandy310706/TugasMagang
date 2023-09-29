bbba1f44d26d4d994d9cf8c0e810d9c702f1d0af
Sandy310706 <benediktussandy31@gmail.com>
2023-09-29 11:28:36 +0700

    Login bekerja

--- resources/views/layouts/Auth/app.blade.php
+++ resources/views/layouts/Auth/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,200;6..12,500&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/c0dc21dad4.js" crossorigin="anonymous"></script>
     <title>@yield('title')</title>
 </head>
 <body class="bg-teal-300 bg-opacity-30">
