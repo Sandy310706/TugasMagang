3e2898c22ba755de4d76f986e3935c397eac792e
Robinsilalahi <silalahirobin0@gmail.com>
2023-10-03 09:33:34 +0700

    keranjang

---
+++ database/migrations/2023_09_29_035815_create_keranjangs_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('keranjangs', function (Blueprint $table) {
+            $table->id();
+            $table->string('user_id')->constrained('user');
+            $table->foreignId('menu_id');
+            $table->foreign('menu_id')->references('id')->on('menus')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('keranjangs');
+    }
+};
