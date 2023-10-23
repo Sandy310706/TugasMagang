a3d0e4ca2e515095ff88e68c627614addd8e4636
Robinsilalahi <silalahirobin0@gmail.com>
2023-10-23 14:55:59 +0700

    subtotal

---
+++ app/Models/keranjangPivot.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class keranjangPivot extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['keranjang_id' , 'user_id' , 'total'];
+
+    public function keranjang()
+    {
+        return $this->hasOne('App\Models\Keranjangs', 'keranjang_id');
+    }
+
+    public function menu()
+    {
+        return $this->hasOne('App\Models\Menu','menu_id');
+    }
+}
