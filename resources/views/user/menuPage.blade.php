0aa5306ea1f28dbda7f734a02656557bdec5b894
frede13 <adventinofrderick@gmail.com>
2023-10-27 11:01:56 +0700

    yy

--- resources/views/user/menuPage.blade.php
+++ resources/views/user/menuPage.blade.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="id">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Amaranth&family=Merriweather:wght@300&family=Oswald:wght@200&family=Rock+Salt&family=Satisfy&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Amaranth&family=Merriweather:wght@300&family=Oswald:wght@200&family=Righteous&family=Rock+Salt&family=Satisfy&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Amaranth&family=Merriweather:wght@300&family=Oswald:wght@200&family=Rock+Salt&family=Satisfy&display=swap"
+        rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Amaranth&family=Merriweather:wght@300&family=Oswald:wght@200&family=Righteous&family=Rock+Salt&family=Satisfy&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="template/menuPage/css/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
@@ -27,33 +33,44 @@
                 <ul class="navbar-nav text-uppercase">
                     <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
                     <div class="keranjangs">
-                        <li class="nav-item"><a class="nav-link" href="/carts"><i class="bi bi-cart"></i>Keranjang</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/carts"><i class="bi bi-cart"></i>Keranjang</a>
+                        </li>
                         <div class="ntif">
                             <p>{{ $data }}</p>
                         </div>
                     </div>
-                       @if (auth())
-                         <li class="nav-item"><a class="nav-link" href="/logout"><i class="bi bi-box-arrow-in-right"></i>Log Out</a></li>
-                       @else
-                         <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
+                    @if (auth())
+                        <li class="nav-item"><a class="nav-link" href="/logout"><i
+                                    class="bi bi-box-arrow-in-right"></i>Log Out</a></li>
+                    @else
+                        <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
                     @endif
                 </ul>
             </div>
         </div>
     </nav>
     <div class="card-container">
-            <h1 class="makanan text-center">Makanan</h1>
-            <div class="alert" id="alerts" style="display: none">Pesanan sudah masuk keranjang</div>
+        <h1 class="makanan text-center">Makanan</h1>
+        <div class="alert" id="alerts" style="display: none">Pesanan sudah masuk keranjang</div>
         <div class="card-menu">
             @foreach ($makanan as $makanans)
                 <div style="display: inline" id="menu-card">
                     <div class="card-menu">
                         <div class="card">
-                            <img src="{{ asset('storage/fileMenu/' . $makanans->foto) }}" alt="">
-                            <div class="kontent">
-                                <h3>{{$makanans->nama}}</h3>
-                                <p>Rp.{{$makanans->harga}}</p>
-                                <button type="submit" onclick="inputData(this)" class="btn btn-submit" data-id="{{ $makanans->id }}">Pesan</button>
+                            <div class="cards">
+                                <div class="image">
+                                    <img src="{{ asset('storage/fileMenu/' . $makanans->foto) }}" alt="">
+                                </div>
+                                <div class="kontents">
+                                    <div class="kontent">
+                                        <h3>{{ $makanans->nama }}</h3>
+                                        <p>Rp.{{ $makanans->harga }}</p>
+                                    </div>
+                                </div>
+                                <div class="clicks">
+                                    <button type="submit" onclick="inputData(this)" class="btn btn-submit"
+                                        data-id="{{ $makanans->id }}">Pesan</button>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -69,12 +86,20 @@
                 <div style="display:inline;">
                     <div class="card-menu">
                         <div class="card">
-                            <img src="{{ asset('storage/fileMenu/' . $minum->foto) }}" alt="">
-                            <div class="kontent">
-                                <h3>{{$minum->nama}}</h3>
-                                <p>{{$minum->harga}}</p>
-                                <div class="kontent">
-                                       <button type="submit" class="btn" id="showAutoCloseAlert"onclick="inputData(this)" data-id="{{ $minum->id }}">Pesan</button>
+                            <div class="cards">
+                                <div class="image">
+                                    <img src="{{ asset('storage/fileMenu/' . $minum->foto) }}" alt="">
+                                </div>
+                                <div class="kontents">
+                                    <div class="kontent">
+                                        <h3>{{ $minum->nama }}</h3>
+                                        <p>{{ $minum->harga }}</p>
+                                    </div>
+                                </div>
+                                <div class="clicks">
+                                    <button type="submit" class="btn btn-submit"
+                                    id="showAutoCloseAlert"onclick="inputData(this)"
+                                    data-id="{{ $minum->id }}">Pesan</button>
                                 </div>
                             </div>
                         </div>
@@ -85,38 +110,40 @@
     </div>
 
     <div class="footer-containers">
-    <footer class="footer">
-        <div class="container footer-container">
-            <div class="sosmed">
-                <ul>
-                    <li class="my-2"><a href=""><i class="bi bi-envelope-fill"></i> SMKN7@gmail.com</a></li>
-                    <li class="my-2"><a href=""><i class="bi bi-telephone-fill"></i> 1244234</a></li>
-                    <li class="my-2"><a href=""><i class="bi bi-instagram"></i> SMKN7Pontianak</a></li>
-                </ul>
+        <footer class="footer">
+            <div class="container footer-container">
+                <div class="sosmed">
+                    <ul>
+                        <li class="my-2"><a href=""><i class="bi bi-envelope-fill"></i> SMKN7@gmail.com</a>
+                        </li>
+                        <li class="my-2"><a href=""><i class="bi bi-telephone-fill"></i> 1244234</a></li>
+                        <li class="my-2"><a href=""><i class="bi bi-instagram"></i> SMKN7Pontianak</a></li>
+                    </ul>
+                </div>
+                <div class="copyright">
+                    <p>Copyright&copy; by Babang Frederick</p>
+                </div>
             </div>
-            <div class="copyright">
-                <p>Copyright&copy; by Babang Frederick</p>
-            </div>
-    </div>
-    </footer>
+        </footer>
     </div>
     <script>
         $(document).ready(function() {
-                    $("#alert").hide();
+            $("#alert").hide();
             $.ajaxSetup({
-                headers:{
+                headers: {
                     'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
             });
         });
-        function inputData(bi){
+
+        function inputData(bi) {
             const id = bi.getAttribute('data-id')
             $.ajax({
                 url: `/carts/${id}`,
                 dataType: "json",
                 type: "POST",
-                data:{},
-                success: function(response){
+                data: {},
+                success: function(response) {
                     location.reload();
                     console.log("berhasil");
                     setTimeout(() => {
@@ -124,13 +151,16 @@
                     }, 3000);
                     document.getElementById('alerts').style.display = 'block';
                 },
-                error: function(){
+                error: function() {
                     console.log('gagal');
                 }
             });
         };
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
     {{-- <script src="script.js/script.js"></script> --}}
 </body>
+
 </html>
