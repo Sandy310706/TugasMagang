5da8be8d3c5f29b022bd87f032137720df26f5c6
frede13 <adventinofrderick@gmail.com>
2023-11-14 10:42:17 +0700

    landing page menu dan semua nya

---
+++ resources/views/layouts/user/nav.blade.php
@@ -0,0 +1,56 @@
+<nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+    <div class="container-fluid d-flex">
+        <div class="menu-toggle">
+            <input type="checkbox">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <div class="navbar-brand ml-5">
+            <h3 class="navbars">Welcome TO......</h3>
+        </div>
+        <div class="navbar-content justift-content-end">
+            <ul class="navbar-nav text-uppercase">
+                <div class="nav-content">
+                    <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+                    @auth
+                        @if (auth()->user()->role == 'admin')
+                            <li class="nav-item"><a class="nav-link" href="/admin/dashboard">Dashboard</a></li>
+                        @endif
+                    @endauth
+                    <li class="nav-item"><a class="nav-link" href="#feedback">Feedback</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/menu">Kantin</a></li>
+                    <div class="keranjangs">
+                        <li class="nav-item"><a class="nav-link" href="/carts"><i
+                                    class="bi bi-cart"></i>Keranjang</a>
+                        </li>
+                        <div class="ntif">
+                            <p></p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="dropdown">
+                    <div class="button-sidebar">
+                        <button class="button-dropdown" onclick="openDropdown()" id="dropdownTrigger">Frederick
+                       <i class="bi bi-caret-right-fill" id="dropdownIcon"></i> </button>
+                    </div>
+                    <div class="dropdown-sidebar" id="dropdownMenu">
+                        <div class="dropdown-content">
+                            <li class="content-dropdown"><a class="nav-dropdown" href="">Akun</li>
+                            <li class="content-dropdown"><a class="nav-dropdown histori" href="/invoice">Histori
+                                    Pesanan</li>
+                            @if (auth())
+                                <li class="content-dropdown"><a class="nav-dropdown" style="padding-top: 20px" href="/logout"><i
+                                            class="bi bi-box-arrow-in-right"></i>Log Out</a></li>
+                            @else
+                                <li class="content-dropdown"><a class="nav-dropdown" href="/login">Login</a></li>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </ul>
+
+        </div>
+    </div>
+</nav>
--- resources/views/user/kantin.blade.php
+++ resources/views/user/kantin.blade.php
@@ -1,57 +1,9 @@
-<!DOCTYPE html>
-<html lang="id">
-
-<head>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Amaranth&family=Merriweather:wght@300&family=Oswald:wght@200&family=Rock+Salt&family=Satisfy&display=swap"
-        rel="stylesheet">
-    <link
-        href="https://fonts.googleapis.com/css2?family=Amaranth&family=Merriweather:wght@300&family=Oswald:wght@200&family=Righteous&family=Rock+Salt&family=Satisfy&display=swap"
-        rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset("css/kantin.css") }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Halaman Kantin</title>
-</head>
-<body>
+@extends('layouts.user.app')
+@section('title','Kantin')
+@section('halamankantin')
     <div class="pembungkus-alert">
-        <div class="custom-alert" id="alerts" style="display: none; font-sans" > pesan sudah ditambahkan </div>
+        <div class="custom-alert" id="alerts" style="display: none; font-sans"> pesan sudah ditambahkan </div>
     </div>
-    <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
-        <div class="container-fluid d-flex">
-            <div class="menu-toggle">
-                <input type="checkbox">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <h3 class="navbar-brand">SMKN7 Pontianak</h3>
-            <div class="justift-content-end">
-                <ul class="navbar-nav text-uppercase">
-                    <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
-                    <div class="keranjangs">
-                        <li class="nav-item"><a class="nav-link" href="/carts"><i class="bi bi-cart"></i>Keranjang</a>
-                        </li>
-                        <div class="ntif">
-                            <p>{{$jumlah}}</p>
-                        </div>
-                    </div>
-                    @if (auth())
-                        <li class="nav-item"><a class="nav-link" href="/logout"><i class="bi bi-box-arrow-in-right"></i>Log Out</a></li>
-                    @else
-                        <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
-                    @endif
-                </ul>
-
-            </div>
-        </div>
-    </nav>
 
     <div class="card-container">
         <h1 class="makanan text-center">Menu</h1>
@@ -69,7 +21,7 @@
                                     <div class="kontent">
                                         <h3>{{ $data->nama }}</h3>
                                         <p>Rp.{{ $data->harga }}</p>
-                                        <p>stok : {{$data->stok}}</p>
+                                        <p>stok : {{ $data->stok }}</p>
                                     </div>
                                 </div>
                                 <div class="clicks">
@@ -81,6 +33,28 @@
                     </div>
                 </div>
             @endforeach
+        </div>
+    </div>
+    <div class="contact page-section">
+        <div class="container contact-form">
+            <div class="text-center mt-5">
+                <h2 class="feedback section-heading text-capatalize mb-5">Feedback</h2>
+            </div>
+            <form method="POST" action="{{ route('Feedback') }}" data-sb-form-api-token="API_TOKEN"
+                style="position: relative; z-index: 9999;">
+                @csrf
+                <div class="row align-items-stretch mb-5">
+                    <div class="col">
+                        <div class="form-group form-group-textarea mb-md-0">
+                            <textarea class="form-control" rows="6" id="feedback" name="feedback" placeholder="Feedback*"
+                                data-sb-validations="required"></textarea>
+                            <div class="invalid-feedback">A Feedback is required.</div>
+                        </div>
+                    </div>
+                </div>
+                <div class="text-end"><button class="button btn btn-info" id="submitButton" type="submit">Kirim
+                        Feedback</button></div>
+            </form>
         </div>
     </div>
     <div class="footer-containers">
@@ -101,46 +75,49 @@
         </footer>
     </div>
 
-        <script>
-            $(document).ready(function(){
-                $("#alert").hide();
-                $.ajaxSetup({
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    }
-                });
-            });
-
-                function inputData(bi) {
-                    const id = bi.getAttribute('data-id')
-                    $.ajax({
-                        url: '/carts/'+ id,
-                        dataType: "json",
-                        type: "POST",
-                        data: {},
-                        success: function(response) {
-                            location.reload();
-                            console.log("berhasil");
-                            setTimeout(() => {
-                                document.getElementById('alerts').ustyle.display = 'none';
-                            }, 10000);
-                            document.getElementById('alerts').style.display = 'block';
-                        },
-                        error: function(error) {
-                            console.log('gagal');
-                            console.log(error)
-                        }
-                    });
+    <script>
+        $(document).ready(function() {
+            $("#alert").hide();
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                 }
+            });
+        });
 
-        </script>
+        function inputData(bi) {
+            const id = bi.getAttribute('data-id')
+            $.ajax({
+                url: '/carts/' + id,
+                dataType: "json",
+                type: "POST",
+                data: {},
+                success: function(response) {
+                    location.reload();
+                    console.log("berhasil");
+                    setTimeout(() => {
+                        document.getElementById('alerts').ustyle.display = 'none';
+                    }, 10000);
+                    document.getElementById('alerts').style.display = 'block';
+                },
+                error: function(error) {
+                    console.log('gagal');
+                    console.log(error)
+                }
+            });
+        }
+    </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
+    </script>
     <script src="script.js/script.js"></script>
     <script src="script.js/scripts.js"></script>
-</body>
+@endsection
 
-</html>
+@push('style')
+    <link rel="stylesheet" href="{{ asset('css/kantin.css') }}">
+@endpush
