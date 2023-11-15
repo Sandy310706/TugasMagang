de2e543212fa0b9e3c91622d86c070b561e76966
Robinsilalahi <silalahirobin0@gmail.com>
2023-11-15 15:21:41 +0700

    LandingPageEND

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -13,9 +13,16 @@ class LandingController extends Controller
     {
         $keranjang = Keranjangs::all();
         $angka = count($keranjang);
-        $user = User::where('role','guest')->first();
-        $makanan = Menu::where('kategori','makanan')->get();
-        $minuman = Menu::where('kategori', 'minuman')->get();
+        $user = User::where('role','guest')
+                    ->orWhere('role','superadmin')
+                    ->orWhere('role', 'admin')
+                    ->first();
+        $makanan = Menu::where('kategori','makanan')
+                        ->take(4)
+                        ->get();
+        $minuman = Menu::where('kategori', 'minuman')
+                        ->take(4)
+                        ->get();
 
 
         return view('user.landingpage',compact('makanan','minuman','angka','user'));
